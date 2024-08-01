5edef23329eda3e356256be664ef19bff3b30538
devhawkz <skydevhawkz@gmail.com>
2024-08-01 08:56:02 +0200

    Added 3 model classes User, Resource, Reservation. Created class library and connected it with API

---
+++ Resursko.Domain/Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿namespace Resursko.Domain.Models;
+public class Reservation
+{
+    public int Id { get; set; }
+    public DateTime StartTime { get; set; } = DateTime.Now;
+    public DateTime EndTime { get; set; }
+    public string? Status { get; set; }
+
+    //relation with Resources
+    public int ResourceId { get; set; }
+    public Resource Resource { get; set; }
+
+    public void CheckStatus()
+    {
+        if (EndTime < DateTime.Now)
+            Status = "inactive";
+
+        else Status = "active";
+    }
+}
---
+++ Resursko.Domain/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Resursko.Domain.Models;
+
+public class User
+{
+    public string FirstName {  get; set; }
+    public string LastName { get; set; }
+
+    public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
