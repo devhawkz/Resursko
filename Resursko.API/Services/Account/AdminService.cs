252c8bdc010a460ba96b0fd037422687770a1663
devhawkz <skydevhawkz@gmail.com>
2024-08-05 12:57:00 +0200

    Small verification of code

--- Resursko.API/Services/Account/AdminService.cs
+++ Resursko.API/Services/Account/AdminService.cs
@@ -2,31 +2,30 @@
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Resursko.API.Services.EmailService;
 
-namespace Resursko.API.Services.Account
+namespace Resursko.API.Services.Account;
+
+public class AdminService(UserManager<User> userManager, AccountServiceHelper serviceHelper, IEmailSenderAsync emailSender) : IAdminService
 {
-    public class AdminService(UserManager<User> userManager, AccountServiceHelper serviceHelper, IEmailSenderAsync emailSender) : IAdminService
+    private const string _roleName = "admin";
+    public async Task<AccountRegistrationResponse> RegisterAdminAsync(AccountRegistrationRequest request)
     {
-        private const string _roleName = "admin";
-        public async Task<AccountRegistrationResponse> RegisterAdminAsync(AccountRegistrationRequest request)
+        var newUser = serviceHelper.GetUser(request);
+        var result = await userManager.CreateAsync(newUser, request.Password!);
+
+        if (!result.Succeeded)
         {
-            var newUser = serviceHelper.GetUser(request);
-            var result = await userManager.CreateAsync(newUser, request.Password!);
-
-            if (!result.Succeeded)
-            {
-                var errors = result.Errors.Select(e => e.Description);
-                return new AccountRegistrationResponse(false, errors);
-            }
-
-            var role = await serviceHelper.GetRoleAsync(_roleName);
-
-            await userManager.AddToRoleAsync(newUser, role.Name!);
-
-            var message = new Message(new string[] { request.Email! }, "User registration", "You have successfully register to our app!");
-
-            await emailSender.SendEmailAsync(message);
-
-            return new AccountRegistrationResponse(true);
+            var errors = result.Errors.Select(e => e.Description);
+            return new AccountRegistrationResponse(false, errors);
         }
+
+        var role = await serviceHelper.GetRoleAsync(_roleName);
+
+        await userManager.AddToRoleAsync(newUser, role.Name!);
+
+        var message = new Message(new string[] { request.Email! }, "User registration", "You have successfully register to our app!");
+
+        await emailSender.SendEmailAsync(message);
+
+        return new AccountRegistrationResponse(true);
     }
 }
