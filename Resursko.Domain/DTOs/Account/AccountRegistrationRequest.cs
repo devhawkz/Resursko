23bf91db73f257ef5070f6a64a74cf0052927305
devhawkz <skydevhawkz@gmail.com>
2024-08-14 14:31:28 +0200

    Added regular expression to AccountRegistration Request model

--- Resursko.Domain/DTOs/Account/AccountRegistrationRequest.cs
+++ Resursko.Domain/DTOs/Account/AccountRegistrationRequest.cs
@@ -15,9 +15,12 @@ public class AccountRegistrationRequest
 
     [Required, EmailAddress]
     public string? Email { get; set; }
+
     [Required, DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\"":{}|<>]).{7,}$", ErrorMessage = "Password must be at least 7 characters long and contain at least one special character and have one uppercase charachter.")]
     public string? Password { get; set; }
 
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\"":{}|<>]).{7,}$", ErrorMessage = "Password must be at least 7 characters long and contain at least one special character.")]
     [Required, DataType(DataType.Password), Compare(nameof(Password))]
     public string? ConfirmPassword {  get; set; } 
 }
